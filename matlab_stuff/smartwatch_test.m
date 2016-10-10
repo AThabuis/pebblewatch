da05896f8b0aa00afa93a6dda8e59f3bc71df755
jeanfrancoisburnier <jean-francois.burnier@epfl.ch>
2016-10-10 17:06:39 +0200

    un peu desordone le code

--- matlab_stuff/smartwatch_test.m
+++ matlab_stuff/smartwatch_test.m
@@ -1,7 +1,5 @@
 clc; clear all; close all;
 
-%%%%%%% TIME PROBLEMS (left foot, right foot)!!!!!
-
 % Loads data file
 load('Data_Matlab_TP/dataTP1.mat');
 
@@ -12,7 +10,7 @@ zdata_200Hz = dataTP.data(:,3);
 Fs_init = 200; %Hz
 Fs      =  10; %Hz
 
-dt = 1/Fs_init; 
+dt = 1/Fs_init;
 t_step = 1/Fs; 
 
 
@@ -34,22 +32,83 @@ u_r = ones(size(right_foot));
 
 tt = 0:t_step:t_end;
 
-plot(tx,xdata,'k'); hold on;
-plot(ty,ydata,'r');
-plot(tz,zdata,'b');
+% Compute moving average mean
+sample_size = 2;
+
+xdata_mean = tsmovavg(xdata,'s',sample_size,1);
+ydata_mean = tsmovavg(ydata,'s',sample_size,1);
+zdata_mean = tsmovavg(zdata,'s',sample_size,1);
+
+xdata_unbiased = xdata_mean;
+ydata_unbiased = ydata_mean;
+zdata_unbiased = zdata_mean;
+
+data_smooth = sqrt(xdata_unbiased.^2+ydata_unbiased.^2+zdata_unbiased.^2);
+data = sqrt(xdata.^2+ydata.^2+zdata.^2);
+
+figure;
+plot(tx, data,'r'); hold on;
+plot(tx, data_smooth,'k');
 plot(t(left_foot),u_l, 'xr');
 plot(t(right_foot),u_r, '+b');
 hold off;
 
 figure;
-plot(t, xdata_200Hz,'k'); hold on;
-plot(t, ydata_200Hz,'r');
-plot(t, zdata_200Hz,'b');
-
-plot(t(left_foot),u_l, 'o');
-plot(t(right_foot),u_r, 'o'); 
-
+hold on;
+%plot(tx,xdata,'k');
+plot(ty,ydata_unbiased,'r');
+%plot(tz,zdata,'b');
+plot(t(left_foot),u_l, 'xr');
+plot(t(right_foot),u_r, '+b');
 hold off;
-title('200Hz');
+title(strcat(num2str(Fs),' Hz'));
 
-legend('X data','Y data','Z data');
+% figure;
+% plot(t, xdata_200Hz,'k'); hold on;
+% plot(t, ydata_200Hz,'r');
+% plot(t, zdata_200Hz,'b');
+% 
+% plot(t(left_foot),u_l, 'o');
+% plot(t(right_foot),u_r, 'o'); 
+% 
+% hold off;
+% title('200Hz');
+
+%legend('X data','Y data','Z data');
+
+% step frequency change this...
+l_step_f = length(left_foot)/(t(left_foot(end))-t(left_foot(1)));
+r_step_f = length(right_foot)/(t(right_foot(end))-t(right_foot(1)));
+
+disp('left foot frequency [Hz]:'); disp(l_step_f);
+disp('right foot frequency [Hz]:'); disp(r_step_f);
+
+% L = length(data_smooth(2:end));
+% NFFT = 2^nextpow2(L);
+% Y = fft(data_smooth(2:end), NFFT)/L;
+% f = Fs/2*linspace(0,1,NFFT/2+1);
+% 
+% figure;
+% plot(f,2*abs(Y(1:NFFT/2+1)));
+
+id_s = find(ty < 4 + 1e-2 & ty > 4 - 1e-2);
+t_s = ty(id_s);
+k = 0;
+while(1)
+    
+    if ty(end-k) <= t_s + 10;
+        break;
+    end
+    k = k+1;
+end
+
+t_e = ty(end-k);
+id_e = find(ty == t_e);
+
+L = length(data_smooth(id_s:id_e));
+NFFT = 2^nextpow2(L);
+Y = fft(data_smooth(id_s:id_e), NFFT)/L;
+f = Fs/2*linspace(0,1,NFFT/2+1);
+
+figure;
+plot(f,2*abs(Y(1:NFFT/2+1)));
