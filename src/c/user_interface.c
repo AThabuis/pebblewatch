4bf20ea27fb006d4fd1517269851931acf4f1965
Jean-Francois Burnier <jean-francois.burnier@epfl.ch>
2016-11-05 11:28:53 +0100

    add comments to user_interface files and change app name

--- src/c/user_interface.c
+++ src/c/user_interface.c
@@ -1,3 +1,18 @@
+/*---------------------------------------------------------------------------
+
+Authors:  Adrien Thabuis
+          Antoine Laurens
+          Jean-François Burnier
+          Hugo Dupont
+          Hugo Viard
+
+Version: 1.0
+Date: 11.2016
+
+user_interface.c
+
+---------------------------------------------------------------------------*/
+
 #include <pebble.h>
 #include "user_interface.h"
 #include "step_frequency.h"
@@ -6,7 +21,7 @@
 #define STRIDE_L_FACTOR 0.00414 //average factor to have the stride length with the height of the user in m
 
 
-// Declare the main window and two text layers
+// Declare the windows and layers
 Window *main_window;
 Window *reset_window;
 Window *step_display_window;
@@ -19,224 +34,80 @@ TextLayer *height_cm_layer;
 TextLayer *steps_layer;
 TextLayer *background_layer;
 
-
+// Menu entry labels
 char* menu_label[4] = {"Start","Step count","Change height","Reset"};
-int user_height[2] = {1,70};
-enum Menu_title {START_COUNT, CHANGE_HEIGHT, RESET};
-int distance_travelled = 0;
+
+// Global varables
+int user_height[2] = {1,70};  // height of user, [meters][centimeters]
+int distance_travelled = 0;   // in meters
+bool first_call = true;       //pedometer first call (after a reset)
+
+// enum to distinguish between the different option of menu
+enum Menu_title {START_COUNT, CHANGE_HEIGHT, RESET, MAX_MENU};
+
+/*
+ * \brief  Update user height display
+ */
 void update_user_height_display(void);
 
-short first_call = 1;  //pedometer first call (after a reset)
+/*
+ * \brief  Callback for reset event
+ */
+void reset_callback(void);
+
+/*
+ * \brief  Callback for down click event
+ */
+void down_single_click_handler(ClickRecognizerRef recognizer, void *context);
+
+/*
+ * \brief  Callback for select click event
+ */
+void select_single_click_handler(ClickRecognizerRef recognizer, void *context);
+
+/*
+ * \brief  Callback for up click event
+ */
+void up_single_click_handler(ClickRecognizerRef recognizer, void *context);
+
+/*
+ * \brief  Configure click callbacks
+ */
+void config_provider(Window *window);
+
+/*
+ * \brief   Callback for menu layer
+ * \return  number of rows for menu layer
+ */
+uint16_t get_num_rows_callback( MenuLayer *menu_layer,  uint16_t section_index, void *context);
+
+/*
+ * \brief   Callback for menu layer
+ * \details Draws the row for a menu layer
+ */
+void draw_row_callback( GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index, void *context);
+
+/*
+ * \brief   Callback for menu layer
+ * \return  Height of cells
+ */
+int16_t get_cell_height_callback( struct MenuLayer *menu_layer, MenuIndex *cell_index, void *context);
+
+/*
+ * \brief     Callback select click
+ * \details   Callback for the menu layer, called
+ *            when a select click event occurs
+ */
+void select_callback( struct MenuLayer *menu_layer,  MenuIndex *cell_index, void *context);
 
 
-
-void update_user_height_display(void)
-{
-    static char m[5];
-    static char cm[5];
-  
-    snprintf(m, 5, "%dm",user_height[H_M]);
-    
-    if (user_height[H_CM] < 10)
-    {
-      snprintf(cm, 5, "0%d",user_height[H_CM]);
-    }
-    else
-    {
-      snprintf(cm, 5, "%d",user_height[H_CM]);
-    }
-  
-    text_layer_set_text(height_m_layer, m);
-    text_layer_set_text(height_cm_layer, cm);
-}
-
-
-
-void update_number_steps_display(const uint16_t steps_number)
-{
-    distance_travelled = (int) (steps_number * (user_height[H_M]*100 + user_height[H_CM]) * STRIDE_L_FACTOR); //in meters
-    
-    static char nb_steps_display[60];
-    if(distance_travelled>=1000)
-    {
-      int kmetres = distance_travelled/1000;
-      int metres = distance_travelled-(1000*kmetres);
-      if(metres<10)
-      {
-        snprintf(nb_steps_display, 60, "Steps:\n%u\nDistance :\n%d.00%d km", steps_number, kmetres, metres);
-      }
-      else if(metres<100)
-      {
-        snprintf(nb_steps_display, 60, "Steps:\n%u\nDistance :\n%d.0%d km", steps_number, kmetres, metres);
-      }
-      else
-      {
-        snprintf(nb_steps_display, 60, "Steps:\n%u\nDistance :\n%d.%d km", steps_number, kmetres, metres);
-      }
-    }
-    else
-    {
-      snprintf(nb_steps_display, 60, "Steps:\n%u\nDistance :\n%d m", steps_number, distance_travelled);
-    }
-    text_layer_set_text(steps_layer, nb_steps_display);
-}
-
-void reset_callback()
-{
-    close_reset_window();
-    window_stack_push(main_window, true);
-}
-
-void down_single_click_handler(ClickRecognizerRef recognizer, void *context)
-{  
-    int user_min_height = 70;
-  
-    if (user_height[H_M]*100+user_height[H_CM] <= user_min_height)
-    {
-        return;  
-    }
-    if(user_height[H_M] >= 0 && user_height[H_CM]-5 < 0)
-    {
-        if(user_height[H_M] == 0)
-        {
-            user_height[H_CM] = 0;
-        }
-        else
-        {
-          user_height[H_CM] = 95;
-          user_height[H_M]--;
-        }
-    }
-    else
-    {
-        user_height[H_CM] -= 5;  
-    }
-    update_user_height_display();
-}
-
-void select_single_click_handler(ClickRecognizerRef recognizer, void *context)
-{
-    close_user_height_window();
-    window_stack_push(main_window, true);
-}
-
-void up_single_click_handler(ClickRecognizerRef recognizer, void *context)
-{
-    if(user_height[H_M]!= 3 && user_height[H_CM]+5 >= 100)
-    {
-        if(user_height[H_M] == 2)
-        {
-            user_height[H_CM] = 95;
-        }
-        else
-        {
-          user_height[H_CM] = 0;
-          user_height[H_M]++;
-        }
-    }
-    else
-    {
-        user_height[H_CM] += 5;
-    }
-    update_user_height_display();
-}
-
-
-
-
-void config_provider(Window *window) 
-{
-  // single click
-  window_single_click_subscribe(BUTTON_ID_UP, up_single_click_handler);
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_single_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_single_click_handler);
-}
-
-
-
-uint16_t get_num_rows_callback( MenuLayer *menu_layer,  uint16_t section_index, void *context)
-{
-  const uint16_t num_rows = 3;
-  return num_rows;
-}
-
-
-
-void draw_row_callback( GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index, void *context)
-{
-  static char s_buff[16];
-  
-  switch (cell_index->row)
-  {
-      case START_COUNT:
-          snprintf(s_buff, sizeof(s_buff), first_call ? menu_label[0]:menu_label[1]);
-          break;
-      
-      case CHANGE_HEIGHT:
-          snprintf(s_buff, sizeof(s_buff), menu_label[2]);
-          break;
-    
-      case RESET:
-          snprintf(s_buff, sizeof(s_buff), menu_label[3]);
-          break;
-    
-      default:
-          snprintf(s_buff, sizeof(s_buff), "Row %d", (int)cell_index->row);
-          break;
-  }
-  
-  // Draw this row's index
-  menu_cell_basic_draw(ctx, cell_layer, s_buff, NULL, NULL);
-}
-
-
-
-int16_t get_cell_height_callback( struct MenuLayer *menu_layer, MenuIndex *cell_index, void *context)
-{
-  const int16_t cell_height = 44;
-  return cell_height;
-}
-
-
-
-void select_callback( struct MenuLayer *menu_layer,  MenuIndex *cell_index, void *context)
-{
-  // Do something in response to the button press
-  switch (cell_index->row)
-  {
-    case START_COUNT:
-      APP_LOG(APP_LOG_LEVEL_INFO, "Index 0\n");
-      open_step_display_window();
-      if(first_call == 1)
-      {
-        init_accel();
-        first_call = 0;
-      }
-      break;
-    case CHANGE_HEIGHT:
-      APP_LOG(APP_LOG_LEVEL_INFO, "Index 1\n");
-      open_user_height_window(); 
-      break;
-    case RESET:
-      APP_LOG(APP_LOG_LEVEL_INFO, "Index 2\n");
-      first_call = 1 ;//We can call again for the "first" time the step counter, accelerometer process functions etc ...
-      reset_n_steps();
-      accel_data_service_unsubscribe();//Stop Accelerometer
-      distance_travelled = 0;
-      open_reset_window();
-      break;
-    default:
-      APP_LOG(APP_LOG_LEVEL_INFO, "Default\n");
-      break;
-  }
-}
-
-
+//---------------------------------
+// Public functions implementation
+//---------------------------------
 
 void open_reset_window()
 {
-  
-    // Create main Window element and assign to pointer
+    // Create reset Window element and assign to pointer
     reset_window = window_create();
     Layer *window_layer = window_get_root_layer(reset_window);  
     GRect bounds = layer_get_bounds(window_layer);
@@ -259,23 +130,24 @@ void open_reset_window()
     layer_add_child(window_layer, text_layer_get_layer(background_layer));
     layer_add_child(window_layer, text_layer_get_layer(reset_layer));
     
-    // Add text
+    // Add text and register callback
     text_layer_set_text(reset_layer, "Reset Done!");
-    app_timer_register(1000, reset_callback, NULL);
+    app_timer_register(1000, reset_callback, NULL); // window will be closed after 1s
   
     // Show the window on the watch, with animated = true
     window_stack_push(reset_window, true);
     return;
 }
 
-
-
 void close_reset_window(void)
 {
-    // Destroy the MenuLayer
+    // Destroy the reset text layer
     text_layer_destroy(reset_layer);
+
+    // Show the window on the watch, with animated = false
     window_stack_push(reset_window, false);
   
+    // Destroy the reset window
     window_destroy(reset_window);
     return;
 }
@@ -283,7 +155,7 @@ void close_reset_window(void)
 void open_step_display_window(void)
 {
   
-    // Create main Window element and assign to pointer
+    // Create step display Window element and assign to pointer
     step_display_window = window_create();
     Layer *window_layer = window_get_root_layer(step_display_window);  
     GRect bounds = layer_get_bounds(window_layer);
@@ -315,22 +187,22 @@ void open_step_display_window(void)
     return;
 }
 
-
 void close_step_display_window(void)
 {
-    // Destroy the MenuLayer
+    // Destroy the steo layer
     text_layer_destroy(steps_layer);
+
+    // Show the window on the watch, with animated = false
     window_stack_push(step_display_window, false);
   
+    // Destroy the step display window
     window_destroy(step_display_window);
     return;
 }
 
-
-
 void open_user_height_window(void)
 {
-    // Create main Window element and assign to pointer
+    // Create user height Window element and assign to pointer
     user_height_window = window_create();
     Layer *window_layer = window_get_root_layer(user_height_window);  
     GRect bounds = layer_get_bounds(window_layer);
@@ -341,12 +213,12 @@ void open_user_height_window(void)
     text_layer_set_background_color(background_layer, GColorWhite);
   
     int padding = 5; // Layer padding
-    int sqr_w = (bounds.size.w-2*padding); // layer width
-    int sqr_h = bounds.size.h/3; // layer height
+    int width = (bounds.size.w-2*padding); // layer width
+    int height = bounds.size.h/3; // layer height
     
     // Create the TextLayer with specific bounds
-    height_m_layer  = text_layer_create(GRect(padding,padding,sqr_w,sqr_h));
-    height_cm_layer = text_layer_create(GRect(padding,2*padding+sqr_h,sqr_w,sqr_h));
+    height_m_layer  = text_layer_create(GRect(padding,padding,width,height));
+    height_cm_layer = text_layer_create(GRect(padding,2*padding+height,width,height));
   
     // Setup layer Information
     text_layer_set_background_color(height_m_layer, GColorBlack);
@@ -376,21 +248,20 @@ void open_user_height_window(void)
     return;
 }
 
-
-
 void close_user_height_window()
 {
-    // Destroy the MenuLayer
+    // Destroy the height layers
     text_layer_destroy(height_m_layer);
     text_layer_destroy(height_cm_layer);
   
+    // Show the window on the watch, with animated = false
     window_stack_push(user_height_window, false);
+
+    // Destroy the user height window
     window_destroy(user_height_window);
   
     return;
 }
-
-
 
 void open_main_window(void)
 {
@@ -421,13 +292,232 @@ void open_main_window(void)
     return;
 }
 
-
-
 void close_main_window(void)
 {
-    // Destroy the MenuLayer
+    // Destroy the menu layer
     menu_layer_destroy(main_menu_layer);
+
+    // Destroy the main window
     window_destroy(main_window);
   
     return;
+}
+
+//---------------------------------
+// Private functions implementation
+//---------------------------------
+
+void update_number_steps_display(const uint16_t steps_number)
+{
+    static char nb_steps_display[60];
+
+    distance_travelled = (int) (steps_number * (user_height[H_M]*100 + user_height[H_CM]) * STRIDE_L_FACTOR);
+    
+    if (distance_travelled>=1000)
+    {
+      int kmetres = distance_travelled/1000;
+      int metres = distance_travelled-(1000*kmetres);
+
+      if (metres<10)
+      {
+        snprintf(nb_steps_display, 60, "Steps:\n%u\nDistance :\n%d.00%d km", steps_number, kmetres, metres);
+      }
+      else if (metres<100)
+      {
+        snprintf(nb_steps_display, 60, "Steps:\n%u\nDistance :\n%d.0%d km", steps_number, kmetres, metres);
+      }
+      else
+      {
+        snprintf(nb_steps_display, 60, "Steps:\n%u\nDistance :\n%d.%d km", steps_number, kmetres, metres);
+      }
+    }
+    else
+    {
+      snprintf(nb_steps_display, 60, "Steps:\n%u\nDistance :\n%d m", steps_number, distance_travelled);
+    }
+
+    // Write the distance in the step layer
+    text_layer_set_text(steps_layer, nb_steps_display);
+}
+
+void update_user_height_display(void)
+{
+    static char m[5];
+    static char cm[5];
+  
+    snprintf(m, 5, "%dm",user_height[H_M]);
+    
+    if (user_height[H_CM] < 10)
+    {
+      snprintf(cm, 5, "0%d",user_height[H_CM]);
+    }
+    else
+    {
+      snprintf(cm, 5, "%d",user_height[H_CM]);
+    }
+  
+    // Write the height in the height layer
+    text_layer_set_text(height_m_layer, m);
+    text_layer_set_text(height_cm_layer, cm);
+
+    return;
+}
+
+void reset_callback(void)
+{
+    close_reset_window();
+
+    // Show the window on the watch, with animated = true
+    window_stack_push(main_window, true);
+    return;
+}
+
+void down_single_click_handler(ClickRecognizerRef recognizer, void *context)
+{  
+    int user_min_height = 70;
+  
+    if (user_height[H_M]*100+user_height[H_CM] <= user_min_height)
+    {
+        // Height cannot be less than min height
+        return;  
+    }
+
+    if (user_height[H_M] >= 0 && user_height[H_CM]-5 < 0)
+    {
+        if (user_height[H_M] == 0)
+        {
+            user_height[H_CM] = 0;
+        }
+        else
+        {
+          user_height[H_CM] = 95;
+          user_height[H_M]--;
+        }
+    }
+    else
+    {
+        user_height[H_CM] -= 5;  
+    }
+
+    update_user_height_display();
+
+    return;
+}
+
+void select_single_click_handler(ClickRecognizerRef recognizer, void *context)
+{
+    close_user_height_window();
+
+    // Show the window on the watch, with animated = true
+    window_stack_push(main_window, true);
+
+    return;
+}
+
+void up_single_click_handler(ClickRecognizerRef recognizer, void *context)
+{
+    if(user_height[H_M]!= 3 && user_height[H_CM]+5 >= 100)
+    {
+        if(user_height[H_M] == 2)
+        {
+            // Height cannont exceed 2.95m
+            user_height[H_CM] = 95;
+        }
+        else
+        {
+          user_height[H_CM] = 0;
+          user_height[H_M]++;
+        }
+    }
+    else
+    {
+        user_height[H_CM] += 5;
+    }
+
+    update_user_height_display();
+
+    return;
+}
+
+void config_provider(Window *window) 
+{
+  // Single click callback
+  window_single_click_subscribe(BUTTON_ID_UP, up_single_click_handler);
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_single_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_single_click_handler);
+
+  return;
+}
+
+uint16_t get_num_rows_callback( MenuLayer *menu_layer,  uint16_t section_index, void *context)
+{
+  const uint16_t num_rows = MAX_MENU;
+  return num_rows;
+}
+
+void draw_row_callback( GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index, void *context)
+{
+  static char s_buff[16];
+  
+  switch (cell_index->row)
+  {
+      case START_COUNT:
+          snprintf(s_buff, sizeof(s_buff), first_call ? menu_label[0]:menu_label[1]);
+          break;
+      
+      case CHANGE_HEIGHT:
+          snprintf(s_buff, sizeof(s_buff), menu_label[2]);
+          break;
+    
+      case RESET:
+          snprintf(s_buff, sizeof(s_buff), menu_label[3]);
+          break;
+    
+      default:
+          snprintf(s_buff, sizeof(s_buff), "Row %d", (int)cell_index->row);
+          break;
+  }
+  
+  // Draw this row's index
+  menu_cell_basic_draw(ctx, cell_layer, s_buff, NULL, NULL);
+  return;
+}
+
+int16_t get_cell_height_callback( struct MenuLayer *menu_layer, MenuIndex *cell_index, void *context)
+{
+    const int16_t cell_height = 44;
+    return cell_height;
+}
+
+void select_callback( struct MenuLayer *menu_layer,  MenuIndex *cell_index, void *context)
+{
+  switch (cell_index->row)
+  {
+    case START_COUNT:
+      open_step_display_window();
+      if(first_call == true)
+      {
+        init_accel();
+        first_call = false;
+      }
+      break;
+
+    case CHANGE_HEIGHT:
+      open_user_height_window(); 
+      break;
+
+    case RESET:
+      first_call = true ;
+      reset_n_steps();
+      accel_data_service_unsubscribe();//Stop Accelerometer
+      distance_travelled = 0;
+      open_reset_window();
+      break;
+
+    default:
+      // Do nothing
+      break;
+  }
+
+  return;
 }
--- src/c/user_interface.h
+++ src/c/user_interface.h
@@ -1,8 +1,13 @@
 /*---------------------------------------------------------------------------
 
-Authors: Adrien Thabuis & Antoine Laurens & J.F. B.
+Authors:  Adrien Thabuis
+          Antoine Laurens
+          Jean-François Burnier
+          Hugo Dupont
+          Hugo Viard
+
 Version: 1.0
-Date: 10.2016
+Date: 11.2016
 
 user_interface.h
 
@@ -11,17 +16,79 @@ user_interface.h
 #ifndef USER_INTERFACE_H
 #define USER_INTERFACE_H
 
+
+/*
+ * \brief     Open the main window
+ * \details   Call this function to open the main window displaying
+ *            a menu (giving the ability to the user to browse between
+ *            the different functionalities of the apps)
+ */
 void open_main_window(void);
+
+/*
+ * \brief     Close the main window
+ * \details   Call this function to close the main window
+ *            and all its layers         
+ */
 void close_main_window(void);
+
+/*
+ * \brief     Open the reset window
+ * \details   Call this function to open a new window displaying
+ *            a message saying that the app has been reset
+ */
 void open_reset_window(void);
+
+/*
+ * \brief     Close the reset window
+ * \details   Call this function to close the reset window
+ *            and all its layers and give the focus to the 
+ *            main window
+ */
 void close_reset_window(void);
+
+/*
+ * \brief     Open the user height window
+ * \details   Call this function to open a new window displaying
+ *            the height of the user (the user may use this window
+ *            to modify the height)
+ */
 void open_user_height_window(void);
+
+/*
+ * \brief     Close the user height window
+ * \details   Call this function to close the height window
+ *            and all its layers and give the focus to the 
+ *            main window          
+ */
 void close_user_height_window(void);
+
+/*
+ * \brief     Open the step display window
+ * \details   Call this function to open a new window displaying
+ *            a steps and the distance traveled
+ */
 void open_step_display_window(void);
+
+/*
+ * \brief     Close the step display window
+ * \details   Call this function to close the step display window
+ *            and all its layers and give the focus to the 
+ *            main window
+ */
 void close_step_display_window(void);
 
+/*
+ * \brief  Update number of step display
+ */
 void update_number_steps_display(uint16_t);
 
+/*
+ * \brief	Enum used to access the user height array
+ * 
+ *	H_M     To access the height in meters
+ *  H_CM  	To access the height in centimeters
+ */
 enum User_height {H_M, H_CM};
 
 #endif
